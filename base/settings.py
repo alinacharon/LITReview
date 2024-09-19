d7a0e06330fe94f38b3f1d90ddd108bf61f8a8ff
Alina Charon <alina.charon@gmail.com>
2024-09-19 16:15:21 +0200

    Update

--- base/settings.py
+++ base/settings.py
@@ -141,7 +141,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'authentication.User'
 
 LOGIN_URL = 'login'
-LOGIN_REDIRECT_URL = 'home'
+LOGIN_REDIRECT_URL = 'feed'
 LOGOUT_REDIRECT_URL = 'home'
 
 MEDIA_URL = '/media/'
