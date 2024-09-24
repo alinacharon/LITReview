983c81f0cb97c3ea13037d5b5d276985c4f992fc
Alina Charon <alina.charon@gmail.com>
2024-09-24 15:36:16 +0200

    Update

--- authentication/forms.py
+++ authentication/forms.py
@@ -4,10 +4,6 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class SignupForm(UserCreationForm):
-    email = forms.EmailField()
-    first_name = forms.CharField(max_length=30, required=False)
-    last_name = forms.CharField(max_length=30, required=False)
-
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
         fields = ('username', 'email', 'first_name', 'last_name', 'password1', 'password2')
--- base/settings.py
+++ base/settings.py
@@ -110,14 +110,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'fr-fr'
-
+LANGUAGE_CODE = 'fr'
 TIME_ZONE = 'Europe/Paris'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
