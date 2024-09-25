af3985aaa9b3bd19ba70fa1c3215cf1bdc8171dc
Alina Charon <alina.charon@gmail.com>
2024-09-25 14:20:58 +0200

    Update

--- reviews/views.py
+++ reviews/views.py
@@ -3,7 +3,6 @@ from django.http import HttpResponseForbidden, HttpResponseBadRequest
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Review, Ticket
-from authentication.models import UserFollows
 from .forms import ReviewForm, TicketForm
 
 
